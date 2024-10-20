ce072552e55c31af16cee3c05005d28c2c0960d3
Praveena <pravee.jks@gmail.com>
2024-10-20 17:52:22 +1100

    testgit

---
+++ src/main/java/week1/day1/HelloTestLeaf.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+import java.util.Random;
+
+public class HelloTestLeaf {
+public static void main(String[] args) {
+	System.out.println("Welcome to TestLeaf");
+	System.out.println(new Random().nextInt(10));
+}
+}
