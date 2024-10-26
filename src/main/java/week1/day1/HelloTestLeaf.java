2c1a068927710b7b37cdc761ef79610647943850
Praveena <pravee.jks@gmail.com>
2024-10-26 19:24:07 +1100

    try

--- src/main/java/week1/day1/HelloTestLeaf.java
+++ src/main/java/week1/day1/HelloTestLeaf.java
@@ -1,10 +1,23 @@
 package week1.day1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
 public class HelloTestLeaf {
-public static void main(String[] args) {
+@Test
+public void runHelloTestLeaf(String[] args) throws MalformedURLException {
+	DesiredCapabilities dc=new DesiredCapabilities();
+	dc.setPlatform(Platform.LINUX);
+	dc.setBrowserName("MicrosoftEdge");
+	RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
 	System.out.println("Welcome to TestLeaf");
 	System.out.println(new Random().nextInt(10));
+	driver.close();
 }
 }
